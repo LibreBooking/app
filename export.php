1ba8a9f933994175d7f7abe4b0ec02355bce5367
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2006-11-20 15:34:41 +0000

    Moved remotely
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@126 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ export.php
@@ -0,0 +1,27 @@
+<?php
+/**
+* Interface form for managing user/group relationships
+* @author Nick Korbel <lqqkout13@users.sourceforge.net>
+* @version 02-23-06
+* @package phpScheduleIt
+*
+* Copyright (C) 2003 - 2006 phpScheduleIt
+* License: GPL, see LICENSE
+*/
+
+include_once('lib/Template.class.php');
+include_once('templates/export.template.php');
+
+$t = new Template(translate('Export'));
+
+$t->printHTMLHeader();
+$t->printWelcome();
+$t->startMain();
+
+
+print_export_table();
+print_jscalendar_setup();
+
+$t->endMain();
+$t->printHTMLFooter();
+?>
