62cda7dd0f043b523a364c5ec13ff74bc545bf04
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2006-11-20 15:43:11 +0000

    Moved remotely
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@139 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ popCalendar.php
@@ -0,0 +1,40 @@
+<?php
+/**
+* Create a new Calendar object and print it
+* @author Nick Korbel <lqqkout13@users.sourceforge.net>
+* @version 06-05-04
+* @package phpScheduleIt
+*
+* Copyright (C) 2003 - 2006 phpScheduleIt
+* License: GPL, see LICENSE
+*/
+/**
+* Calendar class
+*/
+include_once('lib/Calendar.class.php');
+/**
+* Template class
+*/
+include_once('lib/Template.class.php');
+
+// Create Calendar
+$myCal= new Calendar();
+if (isset($_GET['scheduleid']))
+	$myCal->scheduleid = $_GET['scheduleid'];
+	
+$t = new Template($myCal->monthName . ' ' . $myCal->year);
+
+$t->printHTMLHeader();
+$t->startMain();
+?>
+<div align="center" style="margin: 0px; width: 100%;">
+<?php
+// Print calendar
+$myCal->printCalendar();
+?>
+</div>
+<?php
+$t->endMain();
+?>
+	</body>
+</html>
