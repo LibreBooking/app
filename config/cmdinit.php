f291790fbdfa12002ed683e99a963d5be4f49ecb
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2006-11-20 15:21:08 +0000

    Moved remotely
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@108 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ config/cmdinit.php
@@ -0,0 +1,43 @@
+<?php
+/**
+* Command line PHP initialization file.  Please do not edit.
+* @author Nick Korbel <lqqkout13@users.sourceforge.net>
+* @version 03-02-06
+* @package phpScheduleIt
+*/
+/**
+* Please refer to readme.html and LICENSE for any additional information
+*
+* Copyright (C) 2003 - 2006 phpScheduleIt
+* This program is free software; you can redistribute it and/or modify it
+* under the terms of the GNU General Public License as published by the
+* Free Software Foundation; either version 2 of the License, or (at your option)
+* any later version.
+*
+* This program is distributed in the hope that it will be useful, but WITHOUT
+* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+* FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with
+* this program; if not, write to the
+* Free Software Foundation, Inc.
+* 59 Temple Place
+* Suite 330
+* Boston, MA
+* 02111-1307
+* USA
+*/
+
+/********************************************************************/
+/*                   DO NOT CHANGE THIS SECTION                     */
+/********************************************************************/
+
+// Turn off magic quotes (do not edit!)
+set_magic_quotes_runtime(0);
+
+$conf['app']['version'] = '1.2.0 beta';
+
+include_once('constants.php');
+include_once('langs.php');
+/********************************************************************/
+?>
---
+++ config/constants.php
@@ -0,0 +1,34 @@
+<?php
+define('INTERNAL_DATE_SEPERATOR', '/');
+define('RES_TYPE_ADD', 'r');
+define('RES_TYPE_MODIFY', 'm');
+define('RES_TYPE_DELETE', 'd');
+define('RES_TYPE_VIEW', 'v');
+define('RES_TYPE_APPROVE', 'a');
+
+define('INVITE_ACCEPT', 'accept');
+define('INVITE_DECLINE', 'decline');
+
+define('MYCALENDARTYPE_DAY', 1);
+define('MYCALENDARTYPE_WEEK', 2);
+define('MYCALENDARTYPE_MONTH', 3);
+define('MYCALENDARTYPE_SIGNUP', 4);
+
+define('TBL_ADDITIONAL_RESOURCES', 'additional_resources');
+define('TBL_ANNOUNCEMENTS', 'announcements');
+define('TBL_ANONYMOUS_USERS', 'anonymous_users');
+define('TBL_GROUPS', 'groups');
+define('TBL_LOGIN', 'login');
+define('TBL_MUTEX', 'mutex');
+define('TBL_PERMISSION', 'permission');
+define('TBL_REMINDERS', 'reminders');
+define('TBL_RESERVATION_RESOURCES', 'reservation_resources');
+define('TBL_RESERVATION_USERS', 'reservation_users');
+define('TBL_RESERVATIONS', 'reservations');
+define('TBL_RESOURCES', 'resources');
+define('TBL_SCHEDULES', 'schedules');
+define('TBL_USER_GROUPS', 'user_groups');
+
+define('REMINDER_DATE_FORMAT', 'YmdHi');
+define('SECONDS_IN_DAY', 86400);
+?>
