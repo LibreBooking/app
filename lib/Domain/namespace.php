f4a7e3209a0bc343ccee2833bb2a81cf6d1a1cc6
kakkonen <kakkonen@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2010-03-27 21:41:38 +0000

    Fixed domain namespace
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@473 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- lib/Domain/namespace.php
+++ lib/Domain/namespace.php
@@ -7,11 +7,13 @@ require_once(ROOT_DIR . 'lib/Domain/Values/AccountStatus.php');
 require_once(ROOT_DIR . 'lib/Domain/Announcements.php');
 require_once(ROOT_DIR . 'lib/Domain/Schedule.php');
 require_once(ROOT_DIR . 'lib/Domain/ScheduleReservation.php');
-require_once(ROOT_DIR . 'lib/Domain/ResourceOld2.php');
+require_once(ROOT_DIR . 'lib/Domain/Resource.php');
 require_once(ROOT_DIR . 'lib/Domain/ReservationTypes.php');
 require_once(ROOT_DIR . 'lib/Domain/ResourceService.php');
 require_once(ROOT_DIR . 'lib/Domain/ResourcePermissionStore.php');
 require_once(ROOT_DIR . 'lib/Domain/PermissionService.php');
 require_once(ROOT_DIR . 'lib/Domain/SchedulePeriod.php');
 require_once(ROOT_DIR . 'lib/Domain/ScheduleLayout.php');
+
+require_once(ROOT_DIR . 'lib/Domain/AddResource.php');
 ?>
