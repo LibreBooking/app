e5c21a1e4f0fb49689c389d2335f335bb00c4b24
kakkonen <kakkonen@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2010-03-27 21:37:02 +0000

    New AddResource class
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@472 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ lib/Domain/AddResource.php
@@ -0,0 +1,68 @@
+<?php
+require_once(ROOT_DIR . 'lib/Domain/namespace.php');
+
+interface IAddResource
+{
+	/**
+	 * @param string $name
+	 * @param array $additionalFields key value pair of additional fields to use during resource management
+	 */
+	public function AddResource($name, $additionalFields = array());
+}
+
+class AddResource implements IAddResource 
+{	
+	public function AddResource($name, $additionalFields = array())
+	{
+		$name = '';
+		$additionalFields['location'] = '';
+		$additionalFields['contact_info'] = '';
+		$additionalFields['description'] = '';
+		$additionalFields['notes'] = '';
+		$additionalFields['isactive'] = '';
+		$additionalFields['min_duration'] = '';
+		$additionalFields['min_increment'] = '';
+		$additionalFields['max_duration'] = '';
+		$additionalFields['unit_cost'] = '';
+		$additionalFields['autoassign'] = '';
+		$additionalFields['requires_approval'] = '';
+		$additionalFields['allow_multiday'] = '';
+		$additionalFields['max_participants'] = '';
+		$additionalFields['min_notice_time'] = '';
+		$additionalFields['max_notice_time'] = '';
+		$additionalFields['long_quota_id'] = '';
+		$additionalFields['day_quota_id'] = '';
+		
+		$resourceEditCommand = new ResourceEditCommand(
+					$name, 
+					$additionalFields['location'],
+					$additionalFields['contact_info'],
+					$additionalFields['description'], 
+					$additionalFields['notes'], 
+					$additionalFields['isactive'], 
+					$additionalFields['min_duration'], 
+					$additionalFields['min_increment'], 
+					$additionalFields['max_duration'], 
+					$additionalFields['unit_cost'], 
+					$additionalFields['autoassign'], 
+					$additionalFields['requires_approval'], 
+					$additionalFields['allow_multiday'], 
+					$additionalFields['max_participants'], 
+					$additionalFields['min_notice_time'], 
+					$additionalFields['max_notice_time'], 
+					$additionalFields['long_quota_id'], 
+					$additionalFields['day_quota_id']
+					);
+					
+		$userId = ServiceLocator::GetDatabase()->ExecuteInsert($resourceEditCommand);
+		
+		//$this->AutoAssignPermissions($userId);
+	}
+		
+/*	private function AutoAssignPermissions($userId)
+	{
+		$autoAssignCommand = new AutoAssignPermissionsCommand($userId);	
+		ServiceLocator::GetDatabase()->Execute($autoAssignCommand);
+	}*/
+}
+?>
