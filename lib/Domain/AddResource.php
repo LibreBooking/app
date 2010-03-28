2199db5478112d08c6f3eedf7fbb51524dd5c211
kakkonen <kakkonen@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2010-03-28 21:30:15 +0000

    Missing stuff from last commit
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@480 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- lib/Domain/AddResource.php
+++ lib/Domain/AddResource.php
@@ -11,46 +11,81 @@ interface IAddResource
 }
 
 class AddResource implements IAddResource 
-{	
+{
+/*	private $_name;
+	private $_location;
+	private $_contactInfo;
+	private $_description;
+	private $_notes;
+	private $_isActive;
+	private $_minDuration;
+	private $_minIncrement;
+	private $_maxDuration;
+	private $_unitCost;
+	private $_autoAssign;
+	private $_requiresApproval;
+	private $_allowMultiday;
+	private $_maxParticipants;
+	private $_minNotice;
+	private $_maxNotice;
+	
+	private function __construct($name,
+								$location,
+								$contactInfo,
+								$description,
+								$notes,
+								$isActive,
+								$minDuration,
+								$minIncrement,
+								$maxDuration,
+								$autoAssign,
+								$unitCost,
+								$requiresApproval,
+								$allowMultiday,
+								$maxParticipants,
+								$minNotice,
+								$maxNotice)
+	{
+		$this->SetResourceName($resourceName);
+		$this->SetLocation($location);
+		$this->SetContactInfo($contactInfo);
+		$this->SetDescription($description);
+		$this->SetNotes($notes);
+		$this->SetIsActive($isActive);
+		$this->SetMinDuration($minDuration);
+		$this->SetMaxDuration($maxDuration);
+		$this->SetAutoAssign($autoAssign);
+		$this->SetUnitCost($unitCost);
+		$this->SetRequiresApproval($requiresApproval);
+		$this->SetAllowMultiday($allowMultiday);
+		$this->SetMaxParticipants($maxParticipants);
+		$this->SetMinNotice($minNotice);
+		$this->SetMaxNotice($maxNotice);
+	}
+*/
 	public function AddResource($name, $additionalFields = array())
 	{
-		$name = '';
-		$additionalFields['location'] = '';
-		$additionalFields['contact_info'] = '';
-		$additionalFields['description'] = '';
-		$additionalFields['notes'] = '';
-		$additionalFields['isactive'] = '';
-		$additionalFields['min_duration'] = '';
-		$additionalFields['min_increment'] = '';
-		$additionalFields['max_duration'] = '';
-		$additionalFields['unit_cost'] = '';
-		$additionalFields['autoassign'] = '';
-		$additionalFields['requires_approval'] = '';
-		$additionalFields['allow_multiday'] = '';
-		$additionalFields['max_participants'] = '';
-		$additionalFields['min_notice_time'] = '';
-		$additionalFields['max_notice_time'] = '';
-		
-		$resourceEditCommand = new ResourceEditCommand(
+
+		$addResourceCommand = new AddResourceCommand(
 					$name, 
 					$additionalFields['location'],
-					$additionalFields['contact_info'],
+					$additionalFields['contactInfo'],
 					$additionalFields['description'], 
 					$additionalFields['notes'], 
-					$additionalFields['isactive'], 
-					$additionalFields['min_duration'], 
-					$additionalFields['min_increment'], 
-					$additionalFields['max_duration'], 
-					$additionalFields['unit_cost'], 
-					$additionalFields['autoassign'], 
-					$additionalFields['requires_approval'], 
-					$additionalFields['allow_multiday'], 
-					$additionalFields['max_participants'], 
-					$additionalFields['min_notice_time'], 
-					$additionalFields['max_notice_time'] 
+					$additionalFields['isActive'], 
+					$additionalFields['minDuration'], 
+					$additionalFields['minIncrement'], 
+					$additionalFields['maxDuration'], 
+					$additionalFields['unitCost'], 
+					$additionalFields['autoAssign'], 
+					$additionalFields['requiresApproval'], 
+					$additionalFields['allowMultiday'], 
+					$additionalFields['maxParticipants'], 
+					$additionalFields['minNotice'], 
+					$additionalFields['maxNotice'] 
 					);
 					
-		$userId = ServiceLocator::GetDatabase()->ExecuteInsert($resourceEditCommand);
+		$userId = ServiceLocator::GetDatabase()->ExecuteInsert($addResourceCommand);
 		
 		/*$this->AutoAssignPermissions($userId);
 	}
