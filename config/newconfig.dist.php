58cd04305fe0d737297576b3ac5be4aa1e7f5484
telco2001 <telco2001@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-08-12 03:17:02 +0000

    Adding second copy of newconfig as newconfi.dist so that user can get back to the distribution version if needed
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@853 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ config/newconfig.dist.php
@@ -0,0 +1,29 @@
+<?php
+$conf['settings']['server.timezone'] = 'America/Chicago';
+$conf['settings']['allow.self.registration'] = 'true';
+$conf['settings']['admin.email'] = 'admin@example.com';
+$conf['settings']['enable.email'] = 'true';
+$conf['settings']['default.language'] = 'en_US';
+$conf['settings']['allow.rss'] = 'true';
+$conf['settings']['version'] = '2.0.0';
+$conf['settings']['script.url'] = 'http://localhost:8080/';
+$conf['settings']['password.pattern'] = '/^[^\s]{6,}$/i';
+$conf['settings']['schedule']['show.inaccessible.resources'] = 'true';
+$conf['settings']['database']['type'] = 'mysql';
+$conf['settings']['database']['user'] = 'schedule_user';
+$conf['settings']['database']['password'] = 'password';
+$conf['settings']['database']['hostspec'] = '127.0.0.1';
+$conf['settings']['database']['name'] = 'phpscheduleit2';
+$conf['settings']['plugins']['Auth'] = '';
+$conf['settings']['reservation']['notify.created'] = 'true';
+$conf['settings']['phpmailer']['mailer'] = 'mail';
+$conf['settings']['phpmailer']['smtp.host'] = '';
+$conf['settings']['phpmailer']['smtp.port'] = '';
+$conf['settings']['phpmailer']['smtp.secure'] = '';
+$conf['settings']['phpmailer']['smtp.auth'] = '';
+$conf['settings']['phpmailer']['smtp.username'] = '';
+$conf['settings']['phpmailer']['smtp.password'] = '';
+$conf['settings']['image.upload.directory'] = 'Web/uploads/images';		// from root directory
+$conf['settings']['image.upload.url'] = 'uploads/images';				// from script.url
+$conf['settings']['cache.templates'] = 'false';
+?>
