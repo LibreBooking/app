5854fde1cc38368ca2dd49c1b1827c68eb6075d0
telco2001 <telco2001@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-08-12 02:51:54 +0000

    adding is_email call to EmailValidator
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@850 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- lib/Common/Validators/EmailValidator.php
+++ lib/Common/Validators/EmailValidator.php
@@ -1,4 +1,5 @@
 <?php
+require_once(ROOT_DIR . 'lib/Common/Validators/is_email.php');
 
 class EmailValidator extends ValidatorBase implements IValidator 
 {
@@ -11,7 +12,8 @@ class EmailValidator extends ValidatorBase implements IValidator
 
 	public function Validate()
 	{
-		$this->isValid = preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $this->email);
+	//	$this->isValid = preg_match("/^[_\.0-9a-zA-Z-]+@([0-9a-zA-Z][0-9a-zA-Z-]+\.)+[a-zA-Z]{2,6}$/i", $this->email);
+     $this->isValid = is_email($this->email);
 	}
 }
 
