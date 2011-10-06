0ade0e6a4f4d61497f3730accbb5cda069258358
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-10-06 18:29:45 +0000

    renaming authorization to authentication
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@945 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- plugins/Auth/Drupal/Drupal.config.php
+++
@@ -1,3 +0,0 @@
-<?php
-const DRUPAL_DIRECTORY = 'C:/Users/Michael Pinnegar/Desktop/xampp/htdocs/';
-?>
--- plugins/Auth/Ldap/namespace.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/Ldap.config.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/Ldap.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/LdapOptions.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/LdapConfig.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/ILdap.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/LdapUser.php');
-require_once(ROOT_DIR . 'plugins/Auth/Ldap/AdLdapWrapper.php');
-?>
