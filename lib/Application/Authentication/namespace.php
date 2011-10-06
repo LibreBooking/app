a7ab7fac56396481668cd051216dbcb45c061917
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-10-06 16:37:18 +0000

    renaming authorization to authentication
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@941 12d54735-0ee2-4768-9b18-c71e5e561e4c

---
+++ lib/Application/Authentication/namespace.php
@@ -0,0 +1,13 @@
+<?php
+require_once(ROOT_DIR . 'lib/Application/Authentication/IAuthorization.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/Authentication.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/PasswordEncryption.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/Password.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/LoginCookie.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/IRegistration.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/Registration.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/IRegistrationMini.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/RegistrationMini.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/RegistrationAdmin.php');
+require_once(ROOT_DIR . 'lib/Application/Authentication/Reporting.php');
+?>
