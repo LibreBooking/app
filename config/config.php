e2933932f1c742295e795e83a0f04befd5c6b284
dunnleaddress <dunnleaddress@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-11-21 22:48:17 +0000

    Testing LDAP so to write up instruction for it. But test was not successful. Wrote to Nick. I do not usually remove the original code but comment them out.
    Dung.
    
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1052 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- config/config.php
+++ config/config.php
@@ -2,23 +2,23 @@
 error_reporting(E_ALL & ~E_NOTICE);
 
 $conf['settings']['server.timezone'] = 'America/Chicago';   // look up here http://php.net/manual/en/timezones.php
-$conf['settings']['allow.self.registration'] = 'true';  // this is email self registration on home page
-$conf['settings']['admin.email'] = 'admin@example.com'; // notification email to admin
+$conf['settings']['allow.self.registration'] = 'true';  // this is email self registration on home page the other is optional LDAP authentication. You may have both at the same time
+$conf['settings']['admin.email'] = 'admin@example.com'; // notification email to be sent to admin user
 $conf['settings']['default.page.size'] = '50';  // number of records per page
-$conf['settings']['enable.email'] = 'true';
+$conf['settings']['enable.email'] = 'true'; // sending auto email to users
 $conf['settings']['default.language'] = 'en_US';    // find your language in phpScheduleIt/lang directory
-$conf['settings']['allow.rss'] = 'true';
-$conf['settings']['version'] = '2.0.0';
+$conf['settings']['allow.rss'] = 'true';    // rss
+$conf['settings']['version'] = '2.0.0'; // latest
 $conf['settings']['script.url'] = 'localhost';   // http://localhost/';
 $conf['settings']['password.pattern'] = '/^[^\s]{6,}$/i';
 $conf['settings']['schedule']['show.inaccessible.resources'] = 'true';
 $conf['settings']['database']['type'] = 'mysql';
-$conf['settings']['database']['user'] = 'schedule_user';
-$conf['settings']['database']['password'] = 'password';
+$conf['settings']['database']['user'] = 'schedule_user';    // default user
+$conf['settings']['database']['password'] = 'password'; // default password
 $conf['settings']['database']['hostspec'] = '127.0.0.1';
 $conf['settings']['database']['name'] = 'phpscheduleit2';
 $conf['settings']['reservation']['notify.created'] = 'true';    // notifying a booking event
-$conf['settings']['phpmailer']['mailer'] = 'mail';
+$conf['settings']['phpmailer']['mailer'] = 'mail';  // type of mail server
 $conf['settings']['phpmailer']['smtp.host'] = '';   // 'smtp.university.ca'
 $conf['settings']['phpmailer']['smtp.port'] = '';   // '25'
 $conf['settings']['phpmailer']['smtp.secure'] = ''; // 'ssl'
@@ -28,7 +28,7 @@ $conf['settings']['phpmailer']['smtp.password'] = '';   // 'password'
 $conf['settings']['image.upload.directory'] = 'Web/uploads/images'; // from root directory, ie public_html/phpscheduleit/Web/uploads/images should be set to Web/uploads/images
 $conf['settings']['image.upload.url'] = 'uploads/images';   // from script.url
 $conf['settings']['cache.templates'] = 'true';
-$conf['settings']['plugins']['Authentication'] = '';
+$conf['settings']['plugins']['Authentication'] = 'Ldap'; // $conf['settings']['plugins']['Authentication'] = 'Ldap';
 $conf['settings']['plugins']['Authorization'] = '';
 $conf['settings']['plugins']['Permission'] = '';
 $conf['settings']['install.password'] = 'mypass'; // any password
