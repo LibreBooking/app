5fd14d295742a364119ae6d306a6d8afba7b47c2
dunnleaddress <dunnleaddress@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-11-22 21:49:20 +0000

    refining
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1059 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Web/install/index.php
+++ Web/install/index.php
@@ -1,7 +1,9 @@
 <?php
 define('ROOT_DIR', '../../');
 $smartyTemplateCacheDir = ROOT_DIR . 'tpl_c';
-
+/*
+ * 
+ */
 if (SmartyPermissionsAreOk($smartyTemplateCacheDir))
 {
 	require_once(ROOT_DIR . 'Pages/InstallPage.php');
@@ -14,7 +16,9 @@ else
 	echo "The web server must have write access to $smartyTemplateCacheDir";
 	echo "The permissions are currently set to " + substr(sprintf('%o', fileperms($smartyTemplateCacheDir)), -4);
 }
-
+/*
+ * 
+ */
 function SmartyPermissionsAreOk($smartyTemplateCacheDir)
 {
 	if (!is_writable($smartyTemplateCacheDir))
--- misc/todos.php
+++ misc/todos.php
@@ -1,12 +1,14 @@
 <?php
 
 /*
- * To do list. It includes users' requests and the nice-to-have things. Things that contribute values to the project
- *  that are suggested.
+ * This can be compiled in the task list or todos list
+ * 
+ * To do file. It includes users' requests and the nice-to-have things. Things that contribute values to the project
+ *  that are suggested and not included in the inline document of comment sections of tag @todo of source code files.
+ * 
+ * @todo paint a nicer error message box.
+ * @todo configure LDAP.
+ * @todo include sample-data-utf8.sql to the auto database creation.
  */
-
-// TODO: paint a nicer error message box.
-// TODO: configure LDAP.
-// TODO: include sample-data-utf8.sql to the auto database creation.
 
 ?>
