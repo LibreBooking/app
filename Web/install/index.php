f74e94cb32dfc7d8d6f69aea4648552f6ed24853
dunnleaddress <dunnleaddress@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-11-22 23:12:10 +0000

    working on database auto creation script, the goal is to include the sample data by default.
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1060 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Web/install/index.php
+++ Web/install/index.php
@@ -1,31 +1,33 @@
 <?php
+
 define('ROOT_DIR', '../../');
 $smartyTemplateCacheDir = ROOT_DIR . 'tpl_c';
-/*
- * 
- */
-if (SmartyPermissionsAreOk($smartyTemplateCacheDir))
-{
-	require_once(ROOT_DIR . 'Pages/InstallPage.php');
 
-	$page = new InstallPage();
-	$page->PageLoad();
-}
-else
-{
-	echo "The web server must have write access to $smartyTemplateCacheDir";
-	echo "The permissions are currently set to " + substr(sprintf('%o', fileperms($smartyTemplateCacheDir)), -4);
-}
 /*
- * 
+ * Checking directory permission
  */
-function SmartyPermissionsAreOk($smartyTemplateCacheDir)
-{
-	if (!is_writable($smartyTemplateCacheDir))
-	{
-		return chmod($smartyTemplateCacheDir, 0770);
-	}
-        // Why Nick returns false?
-	return true; // false;
+if (SmartyPermissionsAreOk($smartyTemplateCacheDir)) {
+    require_once(ROOT_DIR . 'Pages/InstallPage.php');
+
+    $page = new InstallPage();
+    $page->PageLoad();
+} else {
+    echo "The web server must have write access to $smartyTemplateCacheDir";
+    echo "The permissions are currently set to " + substr(sprintf('%o', fileperms($smartyTemplateCacheDir)), -4);
 }
+
+/*
+ * Determine the permission of given directory
+ * @param string $smartyTemplateCacheDir location of tpl_c directory
+ * @return bool|string bool when writabe, and string otherwise
+ */
+function SmartyPermissionsAreOk($smartyTemplateCacheDir) {
+    if (!is_writable($smartyTemplateCacheDir)) {
+        // Attempt to change permission of directory to 0770 - 1st section 0 for all storage type.
+        return chmod($smartyTemplateCacheDir, 0770);
+    }
+    // @todo Why Nick returns false?
+    return true; // false;
+}
+
 ?>
--- misc/todos.php
+++ misc/todos.php
@@ -2,13 +2,12 @@
 
 /*
  * This can be compiled in the task list or todos list
- * 
+ *
  * To do file. It includes users' requests and the nice-to-have things. Things that contribute values to the project
  *  that are suggested and not included in the inline document of comment sections of tag @todo of source code files.
- * 
+ *
  * @todo paint a nicer error message box.
  * @todo configure LDAP.
  * @todo include sample-data-utf8.sql to the auto database creation.
  */
-
 ?>
