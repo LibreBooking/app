ba9e29c1a884a62e6b9656882db2a7039f371b28
dunnleaddress <dunnleaddress@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-11-23 06:17:02 +0000

    can not echo "string" + "string" instead of "string" . "string"
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1062 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Web/install/index.php
+++ Web/install/index.php
@@ -12,8 +12,8 @@ if (SmartyPermissionsAreOk($smartyTemplateCacheDir)) {
     $page = new InstallPage();
     $page->PageLoad();
 } else {
-    echo "The web server must have write access to $smartyTemplateCacheDir";
-    echo "The permissions are currently set to " + substr(sprintf('%o', fileperms($smartyTemplateCacheDir)), -4);
+    echo "The web server must have write access to $smartyTemplateCacheDir. ";
+    echo "The permissions are currently set to " . substr(sprintf('%o', fileperms($smartyTemplateCacheDir)), -4);
 }
 
 /*
@@ -24,7 +24,7 @@ if (SmartyPermissionsAreOk($smartyTemplateCacheDir)) {
 function SmartyPermissionsAreOk($smartyTemplateCacheDir) {
     if (!is_writable($smartyTemplateCacheDir)) {
         // Attempt to change permission of directory to 0770 - 1st section 0 for all storage type.
-        return chmod($smartyTemplateCacheDir, 0770);
+        return chmod($smartyTemplateCacheDir, 0770);    // often the attempt will fail
     }
     // @todo Why Nick returns false?
     return true; // false;
