7ec324d3cfdb1c7544377a731048150adfa39639
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2011-12-01 17:55:52 +0000

    reverted changes to get database connectivity restored
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1091 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- config/config.php
+++ config/config.php
@@ -4,6 +4,7 @@ error_reporting(E_ALL & ~E_NOTICE);
  * Application configuration
  */
 $conf['settings']['server.timezone'] = 'America/Edmonton';   // look up here http://php.net/manual/en/timezones.php
+$conf['settings']['allow.self.registration'] = 'false';  // this is email self registration on home page the other is optional LDAP authentication. You may have both at the same time
 $conf['settings']['admin.email'] = 'dule@ucalgary.ca'; // notification email to be sent to admin user
 $conf['settings']['default.page.size'] = '50';  // number of records per page
 $conf['settings']['enable.email'] = 'true'; // sending auto email to users
@@ -23,8 +24,8 @@ $conf['settings']['cache.templates'] = 'true';  // caching template files helps 
 $conf['settings']['database']['type'] = 'mysql';
 $conf['settings']['database']['user'] = 'schedule_user';    // default user
 $conf['settings']['database']['password'] = 'password'; // default password
-$conf['settings']['database']['hostspec'] = 'localhost';    // mysql.acs.university.ca
-$conf['settings']['database']['name'] = 'development';
+$conf['settings']['database']['hostspec'] = '127.0.0.1';    // mysql.acs.university.com
+$conf['settings']['database']['name'] = 'phpscheduleit2';
 /**
  * Mail server configuration
  *
@@ -51,9 +52,8 @@ $conf['settings']['phpmailer']['smtp.password'] = '';   // 'password'
 $conf['settings']['plugins']['Authentication'] = '';
 $conf['settings']['plugins']['Authorization'] = '';
 $conf['settings']['plugins']['Permission'] = '';
-$conf['settings']['allow.self.registration'] = 'false';  // this is email self registration on home page, the other is optional LDAP authentication. Can be both.
 /**
  * Installation settings
  */
-$conf['settings']['install.password'] = 'pass';
+$conf['settings']['install.password'] = '';
 ?>
--- lib/Database/MySQL/MySqlConnection.php
+++ lib/Database/MySQL/MySqlConnection.php
@@ -1,111 +1,125 @@
 <?php
 
-class MySqlConnection implements IDbConnection {
+class MySqlConnection implements IDbConnection
+{
+	private $_dbUser = '';
+	private $_dbPassword = '';
+	private $_hostSpec = '';
+	private $_dbName = '';
+	
+	private $_db = null;
+	private $_connected = false;
+	
+	public function __construct($_dbUser, $_dbPassword, $_hostSpec, $_dbName) 
+	{
+		//echo "type: $_dbType user: $_dbUser password: $_dbPassword host: $_hostSpec dbname: $_dbName";
+		$this->_dbUser = $_dbUser;
+		$this->_dbPassword = $_dbPassword;
+		$this->_hostSpec = $_hostSpec;
+		$this->_dbName = $_dbName;
+	}
+	
+	public function Connect() 
+	{
+		if ($this->_connected && !is_null($this->_db)) 
+		{
+			return;
+		}
+		
+		$this->_db = mysql_connect($this->_hostSpec, $this->_dbUser, $this->_dbPassword );
+		mysql_select_db($this->_dbName, $this->_db);
+		
+		if (!$this->_db) 
+		{
+			throw new Exception("Error connecting to database\nError: " . mysql_error());
+			Log::Error("Error connecting to database\n%s",  mysql_error());
+		}
+        
+		$this->_connected = true;
+	}
+	
+	public function Disconnect() 
+	{
+		mysql_close($this->_db); 
+		$this->_db = null;		
+		$this->_connected = false;
+	}
+	
+	public function Query(ISqlCommand $sqlCommand) 
+	{
+		$mysqlCommand = new MySqlCommandAdapter($sqlCommand);
 
-    private $_dbUser = '';
-    private $_dbPassword = '';
-    private $_hostSpec = '';
-    private $_dbName = '';
-    private $_db = null;
-    private $_connected = false;
+		Log::Sql('MySql Query: ' . str_replace('%', '%%', $mysqlCommand->GetQuery()));
 
-    public function __construct($_dbUser, $_dbPassword, $_hostSpec, $_dbName) {
-        // echo "type: $_dbType user: $_dbUser password: $_dbPassword host: $_hostSpec dbname: $_dbName";
-        $this->_dbUser = $_dbUser;
-        $this->_dbPassword = $_dbPassword;
-        $this->_hostSpec = $_hostSpec;
-        $this->_dbName = $_dbName;
-    }
+		$result = mysql_query($mysqlCommand->GetQuery());
+		
+		$this->_handleError($result);
+		
+		return new MySqlReader($result);
+	}
 
-    public function Connect() {
-        if ($this->_connected && !is_null($this->_db)) {
-            return;
-        }
+	public function LimitQuery(ISqlCommand $command, $limit, $offset = 0)
+	{
+		return $this->Query(new MySqlLimitCommand($command, $limit, $offset));
+	}
+	
+	public function Execute(ISqlCommand $sqlCommand) 
+	{
+		$mysqlCommand = new MySqlCommandAdapter($sqlCommand);
 
-        $this->_db = mysql_connect($this->_hostSpec, $this->_dbUser, $this->_dbPassword);
-        mysql_select_db($this->_dbName, $this->_db);
+		Log::Sql('MySql Execute: ' . str_replace('%', '%%', $mysqlCommand->GetQuery()));
 
-        if (!$this->_db) {
-            throw new Exception("Error connecting to database\nError: " . mysql_error());
-            Log::Error("Error connecting to database\n%s", mysql_error());
-        }
-
-        $this->_connected = true;
-    }
-
-    public function Disconnect() {
-        mysql_close($this->_db);
-        $this->_db = null;
-        $this->_connected = false;
-    }
-
-    public function Query(ISqlCommand $sqlCommand) {
-        $mysqlCommand = new MySqlCommandAdapter($sqlCommand);
-
-        Log::Sql('MySql Query: ' . str_replace('%', '%%', $mysqlCommand->GetQuery()));
-
-        $result = mysql_query($mysqlCommand->GetQuery());
-
-        $this->_handleError($result);
-
-        return new MySqlReader($result);
-    }
-
-    public function LimitQuery(ISqlCommand $command, $limit, $offset = 0) {
-        return $this->Query(new MySqlLimitCommand($command, $limit, $offset));
-    }
-
-    public function Execute(ISqlCommand $sqlCommand) {
-        $mysqlCommand = new MySqlCommandAdapter($sqlCommand);
-
-        Log::Sql('MySql Execute: ' . str_replace('%', '%%', $mysqlCommand->GetQuery()));
-
-        $result = mysql_query($mysqlCommand->GetQuery());
-
-        $this->_handleError($result);
-    }
-
-    public function GetLastInsertId() {
-        return mysql_insert_id($this->_db);
-    }
-
-    private function _handleError($result, $sqlCommand = null) {
-        if (!$result) {
-            if ($sqlCommand != null) {
-                echo $sqlCommand->GetQuery();
-            }
-            throw new Exception('There was an error executing your query\n' . mysql_error());
-
-            Log::Error("Error executing MySQL query %s", mysql_error());
-        }
+		$result = mysql_query($mysqlCommand->GetQuery());
+		
+		$this->_handleError($result);
+	}
+	
+	public function GetLastInsertId()
+	{
+		return mysql_insert_id($this->_db);
+	}
+	
+	private function _handleError($result, $sqlCommand = null) 
+	{
+		if (!$result) 
+		{
+			if ($sqlCommand != null)
+			{
+				echo $sqlCommand->GetQuery();
+			}
+			throw new Exception('There was an error executing your query\n' .  mysql_error());
+		
+           	Log::Error("Error executing MySQL query %s",  mysql_error());
+		}
         return false;
-    }
-
+	}
 }
 
-class MySqlLimitCommand extends SqlCommand {
+class MySqlLimitCommand extends SqlCommand
+{
+	/**
+	 * @var \ISqlCommand
+	 */
+	private $baseCommand;
 
-    /**
-     * @var \ISqlCommand
-     */
-    private $baseCommand;
-    private $limit;
-    private $offset;
+	private $limit;
+	private $offset;
 
-    public function __construct(ISqlCommand $baseCommand, $limit, $offset) {
-        parent::__construct();
+	public function __construct(ISqlCommand $baseCommand, $limit, $offset)
+	{
+		parent::__construct();
+		
+		$this->baseCommand = $baseCommand;
+		$this->limit = $limit;
+		$this->offset = $offset;
 
-        $this->baseCommand = $baseCommand;
-        $this->limit = $limit;
-        $this->offset = $offset;
-
-        $this->Parameters = $baseCommand->Parameters;
-    }
-
-    public function GetQuery() {
-        return $this->baseCommand->GetQuery() . sprintf(" LIMIT %s OFFSET %s", $this->limit, $this->offset);
-    }
+		$this->Parameters = $baseCommand->Parameters;
+	}
+	
+	public function GetQuery()
+	{
+		return $this->baseCommand->GetQuery() . sprintf(" LIMIT %s OFFSET %s",  $this->limit, $this->offset);
+	}
 
 }
-
 ?>
