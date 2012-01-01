4510e2b6e654025b6211e40e447c7979f609f932
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-01-01 23:33:32 +0000

    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1166 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- tests/fakes/FakeReservation.php
+++ tests/fakes/FakeReservation.php
@@ -1,4 +1,23 @@
 <?php
+/**
+Copyright 2011-2012 Nick Korbel
+
+This file is part of phpScheduleIt.
+
+phpScheduleIt is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+phpScheduleIt is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with phpScheduleIt.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 $BASE_DIR = dirname(__FILE__) . '/../..';
 require_once($BASE_DIR . '/lib/Reservation.class.php');
 require_once('FakeResource.php');
