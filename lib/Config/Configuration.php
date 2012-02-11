ec5ec00c0f768c1df8e93cea91ee0748ca6cdf06
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-02-11 17:44:59 +0000

    fixing version number
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1227 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- lib/Config/Configuration.php
+++ lib/Config/Configuration.php
@@ -68,7 +68,7 @@ class Configuration implements IConfiguration
 	const DEFAULT_CONFIG_ID = 'phpscheduleit';
 	const DEFAULT_CONFIG_FILE_PATH = 'config/config.php';
 
-    const VERSION = '2.0.0';
+    const VERSION = '2.0';
 
 	protected function __construct()
 	{
