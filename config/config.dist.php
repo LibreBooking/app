be0c797bd20fff2a9a75eae616c7cd277c06c586
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-02-15 13:44:33 +0000

    hide strict warnings
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1238 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- config/config.dist.php
+++ config/config.dist.php
@@ -18,7 +18,7 @@ You should have received a copy of the GNU General Public License
 along with phpScheduleIt.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-error_reporting(E_ALL & ~E_NOTICE);
+error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 /**
  * Application configuration
  */
