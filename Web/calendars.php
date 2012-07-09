6ca00b65477a1322d36642ba0d52394e28c9becd
schloegl <schloegl@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-07-09 21:23:06 +0000

    list of exported calendar: resources are ordered according to schedule; use of public_id (instead of real id);
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1560 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Web/calendars.php
+++ Web/calendars.php
@@ -27,19 +27,28 @@ $username = $conf['settings']['database']['user'];
 $password = $conf['settings']['database']['password'];  
 $database = $conf['settings']['database']['name'];  
 $icskey   = $conf['settings']['ics']['subscription.key'];
+$tab = '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
 
 header("Exported calendars of phpScheduleIt");
 
 echo '<H1>Exported calendars of <a href="',$url,'">phpScheduleIt</a></H1>';
 echo 'Which calendar client are you using ?<br>';
-echo '<a href="',$url,'/',basename($_SERVER['SCRIPT_NAME']),'?outlook=1">Outlook</a><br>';
-echo '<a href="',$url,'/',basename($_SERVER['SCRIPT_NAME']),'?">Mozilla</a><br>';
+foreach (array("Outlook","Mozilla") as $client) {
+    echo $tab.'<a href="'.$url.'/'.basename($_SERVER['SCRIPT_NAME']).'?client='.$client.'">'.$client.'</a><br>';
+}
 
-if ((isset($_GET["outlook"]) && $_GET["outlook"]) 
- || (isset($_GET["win"]) && $_GET["win"]))
+/*
+    Client specific usage description
+ */
+
+if ( ( isset($_GET["outlook"]) && $_GET["outlook"] ) 
+  || ( isset($_GET["win"])     && $_GET["win"] )
+  || ( isset($_GET["client"])  && $_GET["client"]=='Outlook' ) 
+   )
 {
     $url=preg_replace('(^http)m','webcal',$url);
-    echo '<H2>HowTo use with Outlook</H2><pre>Double click on link below to start calendar import into Outlook</pre>';
+    echo '<H2>HowTo use with Outlook</H2>';
+    echo '<pre>Double click on link below to start calendar import into Outlook</pre>';
 }
 else 
 {
@@ -47,8 +56,9 @@ else
     echo '<pre>  Mozilla -> Menu File -> New Calendar -> <br>';
     echo '  ->"Select: On The Network" -> Next<br>';
     echo '  -> Select: iCalendar (ICS), Location: URL starting with https://... ->Next<br>';
-    echo '  -> Name: (its recommended use the name listed below)</pre>';    
+    echo '  -> Name: (it\'s recommended to use the name listed below)</pre>';    
 } 
+
 
 // Open data base connection 
 $link = mysql_connect( $hostname, $username, $password);
@@ -58,17 +68,34 @@ if (!$link) {
 }
 
 // Get Urls for Schedule calendars
-echo "<H3>URLs for all schedules </H3>";
-$result = mysql_query('SELECT public_id, name FROM '.$database.'.schedules ORDER BY name ');
-while ($row = mysql_fetch_assoc($result)) {
-    echo '<a href="',$url,'/export/ical-subscribe.php?uid=&sid=',$row['public_id'],'&rid=&icskey=',$icskey,'">',$row['name'].'</a><br>';
-}
-
-// Get Urls for resource calendars
-$result = mysql_query('SELECT public_id, name FROM '.$database.'.resources ORDER BY name');
-echo "<H3>URLs for resources </H3>";
-while ($row = mysql_fetch_assoc($result)) {
-    echo '<a href="',$url,'/export/ical-subscribe.php?uid=&sid=&rid=',$row['public_id'],'&icskey=',$icskey,'">',$row['name'].'</a><br>';
+echo "<H3>URLs for all schedules and resources </H3>";
+$schedule = mysql_query('SELECT public_id, name, schedule_id AS id, allow_calendar_subscription FROM '.$database.'.schedules ORDER BY name ');
+while ($srow = mysql_fetch_assoc($schedule)) {
+    $sid  = $srow['public_id'];
+    $name = $srow['name'];
+    if ($srow['allow_calendar_subscription'] && $sid)
+    {
+        echo '<b><a href="',$url,'/export/ical-subscribe.php?uid=&sid=',$sid,'&rid=&icskey=',$icskey,'">',$name.'</a></b><br>';
+    }
+    else
+    {
+        echo '<b>',$name.'</b><br>';
+    }
+         
+    // Get Urls for resource calendars
+    $result = mysql_query('SELECT public_id, name, allow_calendar_subscription  FROM '.$database.'.resources WHERE schedule_id = '.mysql_real_escape_string($srow['id']).' ORDER BY name ');
+    while ($rrow = mysql_fetch_assoc($result)) {
+        $rid = $rrow['public_id'];
+        $name= $rrow['name'];
+        if ($rrow['allow_calendar_subscription'] && $rid)
+        {
+            echo $tab,'<a href="',$url,'/export/ical-subscribe.php?uid=&sid=&rid=',$rid,'&icskey=',$icskey,'">',$name.'</a><br>';
+        }
+        else
+        {
+            echo $tab,$name,'<br>';
+        }
+    }
 }
 
 echo "<H3>URLs for accessories </H3>";
@@ -87,6 +114,7 @@ foreach (array("IT%","Announce%","Catering%","Tech%") as $cal) {
 
 // Free the resources associated with the result set
 // This is done automatically at the end of the script
+mysql_free_result($schedule);
 mysql_free_result($result);
 mysql_close($link);
 
