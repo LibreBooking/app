ffae06c50a7194059d1d07182588e2d05aa6631a
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-08-20 19:23:53 +0000

    cleanup manual installation
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1643 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- database_schema/create-user.sql
+++ database_schema/create-user.sql
@@ -1,1 +1,2 @@
+
 GRANT ALL on phpscheduleit2.* to 'schedule_user'@'127.0.0.1' identified by 'password';
--- phing-tasks/CombineDbFilesTask.php
+++ phing-tasks/CombineDbFilesTask.php
@@ -94,7 +94,7 @@ class CombineDbFilesTask extends Task
 		// schema
 		$schemaHandle = fopen($this->schemaFile, "a");
 		$upgradeSchema = $this->GetSchemaFileContents($upgradeDir);
-		$newContents = "$versionInfo\r\n\r\n$upgradeSchema";
+		$newContents = "$versionInfo\r\n\r\n$upgradeSchema\r\n\r\n";
 
 		fwrite($schemaHandle, $newContents);
 		fclose($schemaHandle);
@@ -102,7 +102,7 @@ class CombineDbFilesTask extends Task
 		// data
 		$dataHandle = fopen($this->dataFile, "a");
 		$upgradeData = $this->GetDataFileContents($upgradeDir);
-		$newContents = "$versionInfo\r\n\r\n$upgradeData";
+		$newContents = "$versionInfo\r\n\r\n$upgradeData\r\n\r\n";
 
 		fwrite($dataHandle, $newContents);
 		fclose($dataHandle);
@@ -115,7 +115,7 @@ class CombineDbFilesTask extends Task
 		$upgradeSchema = $this->GetSchemaFileContents($upgradeDir);
 		$upgradeData = $this->GetDataFileContents($upgradeDir);
 
-		fwrite($upgradeHandle, "$upgradeSchema\r\n\r\n$upgradeData");
+		fwrite($upgradeHandle, "$upgradeSchema\r\n\r\n$upgradeData\r\n\r\n");
 		fclose($upgradeHandle);
 	}
 
