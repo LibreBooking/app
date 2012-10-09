82e0fc0265a06467463aaed850b4e705f29b9b49
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2012-10-09 17:45:49 +0000

    changed calendars.php to only work if you manually allow it. otherwise this is a security hole
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1717 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Web/calendars.php
+++ Web/calendars.php
@@ -17,6 +17,7 @@ You should have received a copy of the GNU General Public License
 along with phpScheduleIt.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+die('To use this file, please remove this line from source');
 
 require_once('../config/config.php');
 
