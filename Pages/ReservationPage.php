bfeb22cd864b215e818d78f594245cbd2ab3fcaf
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2013-02-08 21:16:32 +0000

    disabled reminders if emails are disabled
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1873 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Pages/ReservationPage.php
+++ Pages/ReservationPage.php
@@ -16,7 +16,7 @@ GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
 along with phpScheduleIt.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ */
 
 require_once(ROOT_DIR . 'Pages/SecurePage.php');
 require_once(ROOT_DIR . 'Presenters/Reservation/ReservationPresenter.php');
@@ -29,7 +29,7 @@ interface IReservationPage extends IPage
 	 * @param $endPeriods array|SchedulePeriod[]
 	 */
 	function BindPeriods($startPeriods, $endPeriods);
-	
+
 	/**
 	 * Set the resources that can be reserved by this user
 	 * @param $resources array|ResourceDto[]
@@ -48,23 +48,23 @@ interface IReservationPage extends IPage
 	 * @param Date $startDate
 	 */
 	function SetSelectedStart(SchedulePeriod $selectedStart, Date $startDate);
-	
+
 	/**
 	 * @param SchedulePeriod $selectedEnd
 	 * @param Date $endDate
 	 */
 	function SetSelectedEnd(SchedulePeriod $selectedEnd, Date $endDate);
-	
+
 	/**
 	 * @param $repeatTerminationDate Date
 	 */
 	function SetRepeatTerminationDate($repeatTerminationDate);
-	
+
 	/**
 	 * @param UserDto $user
 	 */
 	function SetReservationUser(UserDto $user);
-	
+
 	/**
 	 * @param ResourceDto $resource
 	 */
@@ -110,17 +110,17 @@ interface IReservationPage extends IPage
 	 */
 	function SetCanChangeUser($canChangeUser);
 
-    /**
-     * @abstract
-     * @param bool $canShowAdditionalResources
-     */
+	/**
+	 * @abstract
+	 * @param bool $canShowAdditionalResources
+	 */
 	function ShowAdditionalResources($canShowAdditionalResources);
 
-    /**
-     * @abstract
-     * @param bool $canShowUserDetails
-     */
-    function ShowUserDetails($canShowUserDetails);
+	/**
+	 * @abstract
+	 * @param bool $canShowUserDetails
+	 */
+	function ShowUserDetails($canShowUserDetails);
 
 	/**
 	 * @abstract
@@ -153,7 +153,7 @@ abstract class ReservationPage extends Page implements IReservationPage
 	 * @var ReservationInitializerFactory
 	 */
 	protected $initializationFactory;
-	
+
 	public function __construct($title = null)
 	{
 		parent::__construct($title);
@@ -170,65 +170,74 @@ abstract class ReservationPage extends Page implements IReservationPage
 			new ReservationAuthorization(AuthorizationServiceFactory::GetAuthorizationService()),
 			new AttributeRepository(),
 			ServiceLocator::GetServer()->GetUserSession()
-			);
+		);
 
 		$this->presenter = $this->GetPresenter();
 	}
-	
+
 	/**
 	 * @return IReservationPresenter
 	 */
 	protected abstract function GetPresenter();
-	
+
 	/**
 	 * @return string
 	 */
 	protected abstract function GetTemplateName();
-	
+
 	/**
 	 * @return string
 	 */
 	protected abstract function GetReservationAction();
-		
+
 	public function PageLoad()
 	{
 		$this->presenter->PageLoad();
 		$this->Set('ReturnUrl', $this->GetLastPage(Pages::SCHEDULE));
 		$this->Set('ReservationAction', $this->GetReservationAction());
 		$this->Set('MaxUploadSize', UploadedFile::GetMaxSize());
-		$this->Set('UploadsEnabled', Configuration::Instance()->GetSectionKey(ConfigSection::UPLOADS, ConfigKeys::UPLOAD_ENABLE_RESERVATION_ATTACHMENTS, new BooleanConverter()));
-		$this->Set('ShowParticipation', !Configuration::Instance()->GetSectionKey(ConfigSection::RESERVATION, ConfigKeys::RESERVATION_PREVENT_PARTICIPATION, new BooleanConverter()));
-		$this->Set('RemindersEnabled', Configuration::Instance()->GetSectionKey(ConfigSection::RESERVATION, ConfigKeys::RESERVATION_REMINDERS_ENABLED, new BooleanConverter()));
+		$this->Set('UploadsEnabled', Configuration::Instance()->GetSectionKey(ConfigSection::UPLOADS,
+																			  ConfigKeys::UPLOAD_ENABLE_RESERVATION_ATTACHMENTS,
+																			  new BooleanConverter()));
+		$this->Set('ShowParticipation', !Configuration::Instance()->GetSectionKey(ConfigSection::RESERVATION,
+																				  ConfigKeys::RESERVATION_PREVENT_PARTICIPATION,
+																				  new BooleanConverter()));
+		$remindersEnabled = Configuration::Instance()->GetSectionKey(ConfigSection::RESERVATION,
+																	 ConfigKeys::RESERVATION_REMINDERS_ENABLED,
+																	 new BooleanConverter());
+		$emailEnabled = Configuration::Instance()->GetKey(ConfigKeys::ENABLE_EMAIL,
+														  new BooleanConverter());
+		$this->Set('RemindersEnabled', $remindersEnabled && $emailEnabled);
 
 		$this->Set('RepeatEveryOptions', range(1, 20));
-		$this->Set('RepeatOptions', array (
-						'none' => array('key' => 'DoesNotRepeat', 'everyKey' => ''),
-						'daily' => array('key' => 'Daily', 'everyKey' => 'days'),
-						'weekly' => array('key' => 'Weekly', 'everyKey' => 'weeks'),
-						'monthly' => array('key' => 'Monthly', 'everyKey' => 'months'),
-						'yearly' => array('key' => 'Yearly', 'everyKey' => 'years'),
-								)
+		$this->Set('RepeatOptions', array(
+									  'none' => array('key' => 'DoesNotRepeat', 'everyKey' => ''),
+									  'daily' => array('key' => 'Daily', 'everyKey' => 'days'),
+									  'weekly' => array('key' => 'Weekly', 'everyKey' => 'weeks'),
+									  'monthly' => array('key' => 'Monthly', 'everyKey' => 'months'),
+									  'yearly' => array('key' => 'Yearly', 'everyKey' => 'years'),
+								  )
 		);
 		$this->Set('DayNames', array(
-								0 => 'DaySundayAbbr',
-								1 => 'DayMondayAbbr',
-								2 => 'DayTuesdayAbbr',
-								3 => 'DayWednesdayAbbr',
-								4 => 'DayThursdayAbbr',
-								5 => 'DayFridayAbbr',
-								6 => 'DaySaturdayAbbr',
-								)
+								 0 => 'DaySundayAbbr',
+								 1 => 'DayMondayAbbr',
+								 2 => 'DayTuesdayAbbr',
+								 3 => 'DayWednesdayAbbr',
+								 4 => 'DayThursdayAbbr',
+								 5 => 'DayFridayAbbr',
+								 6 => 'DaySaturdayAbbr',
+							 )
 		);
 
 		$this->Display($this->GetTemplateName());
 	}
-	
+
 	public function BindPeriods($startPeriods, $endPeriods)
 	{
 		$this->Set('StartPeriods', $startPeriods);
 		$this->Set('EndPeriods', $endPeriods);
 	}
-	
+
 	public function BindAvailableResources($resources)
 	{
 		$this->Set('AvailableResources', $resources);
@@ -243,13 +252,13 @@ abstract class ReservationPage extends Page implements IReservationPage
 	{
 		$this->Set('AvailableAccessories', $accessories);
 	}
-	
+
 	public function SetSelectedStart(SchedulePeriod $selectedStart, Date $startDate)
 	{
 		$this->Set('SelectedStart', $selectedStart);
 		$this->Set('StartDate', $startDate);
 	}
-	
+
 	public function SetSelectedEnd(SchedulePeriod $selectedEnd, Date $endDate)
 	{
 		$this->Set('SelectedEnd', $selectedEnd);
@@ -265,7 +274,7 @@ abstract class ReservationPage extends Page implements IReservationPage
 		$this->Set('ReservationUserName', $user->FullName());
 		$this->Set('UserId', $user->Id());
 	}
-	
+
 	/**
 	 * @param $resource ResourceDto
 	 * @return void
@@ -275,12 +284,12 @@ abstract class ReservationPage extends Page implements IReservationPage
 		$this->Set('ResourceName', $resource->Name);
 		$this->Set('ResourceId', $resource->Id);
 	}
-	
+
 	public function SetScheduleId($scheduleId)
 	{
 		$this->Set('ScheduleId', $scheduleId);
 	}
-	
+
 	public function SetRepeatTerminationDate($repeatTerminationDate)
 	{
 		$this->Set('RepeatTerminationDate', $repeatTerminationDate);
@@ -311,15 +320,15 @@ abstract class ReservationPage extends Page implements IReservationPage
 		$this->Set('CanChangeUser', $canChangeUser);
 	}
 
-    public function ShowUserDetails($canShowUserDetails)
-    {
-        $this->Set('ShowUserDetails', $canShowUserDetails);
-    }
+	public function ShowUserDetails($canShowUserDetails)
+	{
+		$this->Set('ShowUserDetails', $canShowUserDetails);
+	}
 
 	public function ShowReservationDetails($showReservationDetails)
-    {
-        $this->Set('ShowReservationDetails', $showReservationDetails);
-    }
+	{
+		$this->Set('ShowReservationDetails', $showReservationDetails);
+	}
 
 	public function SetCustomAttributes($attributes)
 	{
@@ -331,4 +340,5 @@ abstract class ReservationPage extends Page implements IReservationPage
 		$this->Set('HideRecurrence', $isHidden);
 	}
 }
+
 ?>
