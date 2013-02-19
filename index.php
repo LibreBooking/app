94ef6def2af8172da8602d575f63fa04d15ec8ae
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2013-02-19 19:17:34 +0000

    seems like the rewrite rule is working
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@1900 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- index.php
+++ index.php
@@ -20,9 +20,9 @@ along with phpScheduleIt.  If not, see <http://www.gnu.org/licenses/>.
 
 require_once('config/config.php');
 
-echo 'Please update the $conf[\'settings\'][\'script.url\'] setting in your config file to be http://' . $_SERVER['SERVER_NAME'] . str_replace('/index.php', '', $_SERVER['PHP_SELF']) . '/Web';
-echo '<br/>You will be redirected automatically in 20 seconds, but portions of phpScheduleIt will not function correctly.';
+#echo 'Please update the $conf[\'settings\'][\'script.url\'] setting in your config file to be http://' . $_SERVER['SERVER_NAME'] . str_replace('/index.php', '', $_SERVER['PHP_SELF']) . '/Web';
+#echo '<br/>You will be redirected automatically in 20 seconds, but portions of phpScheduleIt will not function correctly.';
 
-header( "refresh:20;url=Web" );
+header( "refresh:0;url=Web?{$_SERVER['QUERY_STRING']}" );
 
 ?>
