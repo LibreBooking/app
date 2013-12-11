a4cfadef35962cca7eaa5b0647172dcd4d4485f6
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2013-12-11 19:31:39 +0000

    fixing problem where resource objects weren't loaded with enough data to make approvals work
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@2192 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Domain/Access/ReservationRepository.php
+++ Domain/Access/ReservationRepository.php
@@ -266,8 +266,7 @@ class ReservationRepository implements IReservationRepository
 		$reader = ServiceLocator::GetDatabase()->Query($getResourcesCommand);
 		while ($row = $reader->GetRow())
 		{
-			$resource = new BookableResource($row[ColumnNames::RESOURCE_ID], $row[ColumnNames::RESOURCE_NAME], $row[ColumnNames::RESOURCE_LOCATION], $row[ColumnNames::RESOURCE_CONTACT], $row[ColumnNames::RESOURCE_NOTES], $row[ColumnNames::RESOURCE_MINDURATION], $row[ColumnNames::RESOURCE_MAXDURATION], $row[ColumnNames::RESOURCE_AUTOASSIGN], $row[ColumnNames::RESOURCE_REQUIRES_APPROVAL], $row[ColumnNames::RESOURCE_ALLOW_MULTIDAY], $row[ColumnNames::RESOURCE_MAX_PARTICIPANTS], $row[ColumnNames::RESOURCE_MINNOTICE], $row[ColumnNames::RESOURCE_MAXNOTICE], $row[ColumnNames::RESOURCE_DESCRIPTION], $row[ColumnNames::SCHEDULE_ID]);
-
+			$resource = BookableResource::Create($row);
 			if ($row[ColumnNames::RESOURCE_LEVEL_ID] == ResourceLevel::Primary)
 			{
 				$series->WithPrimaryResource($resource);
