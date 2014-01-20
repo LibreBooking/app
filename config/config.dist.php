7f9aad1ce763ea7b486488582db2575dc7f72335
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2014-01-20 18:09:38 +0000

    comments
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@2260 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- config/config.dist.php
+++ config/config.dist.php
@@ -37,7 +37,7 @@ $conf['settings']['image.upload.directory'] = 'Web/uploads/images'; // full or r
 $conf['settings']['image.upload.url'] = 'uploads/images';       // full or relative path to show uploaded images from
 $conf['settings']['cache.templates'] = 'true';                  // true recommended, caching template files helps web pages render faster
 $conf['settings']['use.local.jquery'] = 'false';                // false recommended, delivers jQuery from Google CDN, uses less bandwidth
-$conf['settings']['registration.captcha.enabled'] = 'true';     // recommended, requires php_gd2 enabled in php.ini
+$conf['settings']['registration.captcha.enabled'] = 'true';     // recommended. unless using recaptcha this requires php_gd2 enabled in php.ini
 $conf['settings']['registration.require.email.activation'] = 'false';		// requires enable.email = true
 $conf['settings']['registration.auto.subscribe.email'] = 'false';			// requires enable.email = true
 $conf['settings']['registration.notify.admin'] = 'false';		// whether the registration of a new user sends an email to the admin (ala phpScheduleIt 1.2)
