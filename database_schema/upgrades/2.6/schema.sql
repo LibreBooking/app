393ca989162ab0e9cec3c10b1d2196df7a435db7
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2014-04-21 17:47:15 +0000

    making reservation_file file_type varchar 75 to accommodate long type names
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@2413 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- database_schema/upgrades/2.6/schema.sql
+++ database_schema/upgrades/2.6/schema.sql
@@ -1,3 +1,5 @@
 ALTER TABLE `custom_attributes` ADD COLUMN `admin_only` tinyint(1) unsigned;
 
 ALTER TABLE  `user_preferences` CHANGE COLUMN `value` `value` text;
+
+ALTER TABLE  `reservation_files` CHANGE COLUMN `file_type` `file_type` varchar(75);
