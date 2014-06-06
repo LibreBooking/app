dad19a959eb9849bdb1de71b3d490225b39c6f4e
lqqkout13 <lqqkout13@12d54735-0ee2-4768-9b18-c71e5e561e4c>
2014-06-06 13:43:02 +0000

    taking out some debugging
    git-svn-id: https://svn.code.sf.net/p/phpscheduleit/code/development@2463 12d54735-0ee2-4768-9b18-c71e5e561e4c

--- Pages/SchedulePage.php
+++ Pages/SchedulePage.php
@@ -251,7 +251,7 @@ class SchedulePage extends ActionPage implements ISchedulePage
 		$this->Set('SlotLabelFactory', $user->IsAdmin ? new AdminSlotLabelFactory() : new SlotLabelFactory($user));
 		$this->Set('DisplaySlotFactory', new DisplaySlotFactory());
 		$this->Set('PopupMonths', $this->IsMobile ? 1 : 3);
-		if (true || $this->IsMobile)
+		if ($this->IsMobile)
 		{
 			$this->Display('Schedule/schedule-mobile.tpl');
 		}
