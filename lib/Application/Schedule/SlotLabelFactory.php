c11e3604e0849a437f441558391084d0260ed3fc
Nick Korbel <nick@twinkletoessoftware.com>
2015-03-27 18:05:34 -0500

    prevent reservation label from being shown when not applicable

--- lib/Application/Schedule/SlotLabelFactory.php
+++ lib/Application/Schedule/SlotLabelFactory.php
@@ -1,19 +1,19 @@
 <?php
+
 /**
-Copyright 2012-2014 Nick Korbel
-
-This file is part of Booked Scheduler is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012-2014 Nick Korbel
+ *
+ * This file is part of Booked Scheduler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 class SlotLabelFactory
 {
 	/**
@@ -24,6 +24,10 @@ class SlotLabelFactory
 	public function __construct($user = null)
 	{
 		$this->user = $user;
+		if ($this->user == null)
+		{
+			$this->user = ServiceLocator::GetServer()->GetUserSession();
+		}
 	}
 
 	/**
@@ -44,6 +48,15 @@ class SlotLabelFactory
 	 */
 	public function Format(ReservationItemView $reservation, $format = null)
 	{
+		$shouldHide = Configuration::Instance()->GetSectionKey(ConfigSection::PRIVACY,
+															   ConfigKeys::PRIVACY_HIDE_USER_DETAILS,
+															   new BooleanConverter());
+
+		if ($shouldHide && (is_null($this->user) || ($this->user->UserId != $reservation->UserId && !$this->user->IsAdminForGroup($reservation->OwnerGroupIds()))))
+		{
+			return '';
+		}
+
 		if (empty($format))
 		{
 			$format = Configuration::Instance()->GetSectionKey(ConfigSection::SCHEDULE, ConfigKeys::SCHEDULE_RESERVATION_LABEL);
