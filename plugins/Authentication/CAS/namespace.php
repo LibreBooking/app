40a80be4264089c0d68836275e98ff50cbabb92c
Nick Korbel <nick@twinkletoessoftware.com>
2015-05-10 10:59:29 -0500

    upgraded CAS to 1.3.3

--- plugins/Authentication/CAS/namespace.php
+++ plugins/Authentication/CAS/namespace.php
@@ -20,7 +20,7 @@ along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 require_once(ROOT_DIR . 'plugins/Authentication/CAS/CAS.php');
-require_once(ROOT_DIR . 'plugins/Authentication/CAS/CAS-1.3.1/CAS.php');
+require_once(ROOT_DIR . 'plugins/Authentication/CAS/CAS-1.3.3/CAS.php');
 require_once(ROOT_DIR . 'plugins/Authentication/CAS/CASOptions.php');
 require_once(ROOT_DIR . 'plugins/Authentication/CAS/CASConfig.php');
 ?>
