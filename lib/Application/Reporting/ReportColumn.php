7876940f02a742c28432fc223a785e38a468e533
Nick Korbel <nick@twinkletoessoftware.com>
2015-12-08 19:22:24 -0600

    include total hours in reports

--- lib/Application/Reporting/ReportColumn.php
+++ lib/Application/Reporting/ReportColumn.php
@@ -1,21 +1,21 @@
 <?php
 /**
-Copyright 2012-2015 Nick Korbel
-
-This file is part of Booked Scheduler.
-
-Booked Scheduler is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-Booked Scheduler is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
+ * Copyright 2012-2015 Nick Korbel
+ *
+ * This file is part of Booked Scheduler.
+ *
+ * Booked Scheduler is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Booked Scheduler is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Booked Scheduler.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 require_once(ROOT_DIR . 'lib/Application/Reporting/ChartColumnDefinition.php');
@@ -221,6 +221,7 @@ class ReportTimeColumn extends ReportColumn
 
 	public function GetData($data)
 	{
-		return new TimeInterval($data);
+		$interval = new TimeInterval($data);
+		return $interval->ToString(true);
 	}
 }
