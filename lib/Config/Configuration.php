fd00598e1f0883ed4796e7813a1bf6d76368925b
Nick Korbel <nick@twinkletoessoftware.com>
2016-08-07 14:53:18 -0500

    bump version number to beta 2

--- lib/Config/Configuration.php
+++ lib/Config/Configuration.php
@@ -81,7 +81,7 @@ class Configuration implements IConfiguration
 	const DEFAULT_CONFIG_ID = 'booked';
 	const DEFAULT_CONFIG_FILE_PATH = 'config/config.php';
 
-    const VERSION = '2.6';
+    const VERSION = '2.6 beta2';
 
 	protected function __construct()
 	{
