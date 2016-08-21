a6cb91a915d80b86dd28a8ac0fa2a4bdbf015e45
Nick Korbel <nick@twinkletoessoftware.com>
2016-08-20 21:51:51 -0500

    fixed bad concat operator

--- index.php
+++ index.php
@@ -23,6 +23,6 @@ require_once('config/config.php');
 #echo 'Please update the $conf[\'settings\'][\'script.url\'] setting in your config file to be http://' . $_SERVER['SERVER_NAME'] . str_replace('/index.php', '', $_SERVER['PHP_SELF']) . '/Web';
 #echo '<br/>You will be redirected automatically in 20 seconds, but portions of Booked Scheduler will not function correctly.';
 
-header( "refresh:0;url=Web?" + urlencode($_SERVER['QUERY_STRING']) );
+header( "refresh:0;url=Web?" . urlencode($_SERVER['QUERY_STRING']) );
 exit;
 ?>
