d153294403e71b52ea26a27a2ad6dd4e9c74b4fe
Nick Korbel <nick@twinkletoessoftware.com>
2016-09-27 21:16:03 -0500

    fixing issue showing whether or not a reservation resource required checkin

--- Web/scripts/reservation.js
+++ Web/scripts/reservation.js
@@ -334,8 +334,8 @@ function Reservation(opts) {
 				var checkedResourceName = $(checkbox).parent().text();
 				var color = $(checkbox).attr('reservation-color');
 				var textColor = $(checkbox).attr('reservation-text-color');
-				var requiresApproval = $(checkbox).attr('requires-approval');
-				var requiresCheckin = $(checkbox).attr('requires-checkin');
+				var requiresApproval = $(checkbox).attr('requires-approval') != '0';
+				var requiresCheckin = $(checkbox).attr('requires-checkin') != '0';
 				var autoReleaseMinutes = $(checkbox).attr('autorelease-minutes');
 
 				if (i === 0)
@@ -343,7 +343,7 @@ function Reservation(opts) {
 					primaryResourceContainer.find('.resourceName').remove();
 					displayDiv = primaryResourceContainer;
 				}
-				displayDiv.append('<div class="resourceName" style="background-color:' + color + '; color:' + textColor + ';">' + '<span class="resourceDetails">' + checkedResourceName + '</span> ' + '<input class="resourceId" type="hidden" name="additionalResources[]" value="' + checkedResourceId + '"/>' + (requiresApproval ? ' <i class="fa fa-lock" data-tooltip="approval"></i> ' : '') + (requiresCheckin ? ' <i class="fa fa-check" data-tooltip="checkin"></i> ' : '') + (_.isEmpty(autoReleaseMinutes) ? ' <i class="fa fa-clock-o" data-tooltip="autorelease" data-autorelease="' + autoReleaseMinutes + '"></i> ' : '') + '</div>');
+				displayDiv.append('<div class="resourceName" style="background-color:' + color + '; color:' + textColor + ';">' + '<span class="resourceDetails">' + checkedResourceName + '</span> ' + '<input class="resourceId" type="hidden" name="additionalResources[]" value="' + checkedResourceId + '"/>' + (requiresApproval ? ' <i class="fa fa-lock" data-tooltip="approval"></i> ' : '') + (requiresCheckin ? ' <i class="fa fa-sign-in" data-tooltip="checkin"></i> ' : '') + (!_.isEmpty(autoReleaseMinutes) ? ' <i class="fa fa-clock-o" data-tooltip="autorelease" data-autorelease="' + autoReleaseMinutes + '"></i> ' : '') + '</div>');
 			});
 		}
 
