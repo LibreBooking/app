c9447607a57c55942b357d7809522722b1fed327
Nick Korbel <nick@twinkletoessoftware.com>
2017-03-22 20:56:57 -0500

    ensure login enforced for subscriptions, if enabled

--- Web/export/atom-subscribe.php
+++ Web/export/atom-subscribe.php
@@ -23,4 +23,8 @@ define('ROOT_DIR', '../../');
 require_once(ROOT_DIR . 'Pages/Export/AtomSubscriptionPage.php');
 
 $page = new AtomSubscriptionPage();
+if (Configuration::Instance()->GetSectionKey('ics', 'require.login', new BooleanConverter()))
+{
+    $page = new SecurePageDecorator($page);
+}
 $page->PageLoad();
--- Web/export/ical-subscribe.php
+++ Web/export/ical-subscribe.php
@@ -23,5 +23,9 @@ define('ROOT_DIR', '../../');
 require_once(ROOT_DIR . 'Pages/Export/CalendarSubscriptionPage.php');
 
 $page = new CalendarSubscriptionPage();
+if (Configuration::Instance()->GetSectionKey('ics', 'require.login', new BooleanConverter()))
+{
+    $page = new SecurePageDecorator($page);
+}
 $page->PageLoad();
 
