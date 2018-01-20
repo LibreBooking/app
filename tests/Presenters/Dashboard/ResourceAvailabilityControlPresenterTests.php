aa8181f60f55cc54d26212d645c7ca8ea584f944
Nick Korbel <nick@twinkletoessoftware.com>
2018-01-20 17:43:48 -0600

    fixed intermittently failing tests

--- tests/Presenters/Dashboard/ResourceAvailabilityControlPresenterTests.php
+++ tests/Presenters/Dashboard/ResourceAvailabilityControlPresenterTests.php
@@ -79,6 +79,7 @@ class ResourceAvailabilityControlPresenterTests extends TestBase
 
 	public function testSetsResourceAvailability()
 	{
+	    Date::_SetNow(Date::Parse('2017-01-20 4:00', 'UTC'));
 	    $this->PopulateSchedules();
 		$this->PopulateResources();
 		$this->PopulateReservations();
