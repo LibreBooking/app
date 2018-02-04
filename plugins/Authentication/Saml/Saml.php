77339050b2ea105a60ece5b495463b12cc1e30c6
Nick Korbel <nick@twinkletoessoftware.com>
2018-02-04 11:02:59 -0600

    lock down profile attributes for external auth

--- plugins/Authentication/Saml/Saml.php
+++ plugins/Authentication/Saml/Saml.php
@@ -214,6 +214,40 @@ class Saml extends Authentication implements IAuthentication
 	{
 		return true;
 	}
-}
 
-?>
+
+    public function AllowUsernameChange()
+    {
+        return false;
+    }
+
+    public function AllowEmailAddressChange()
+    {
+        return false;
+    }
+
+    public function AllowPasswordChange()
+    {
+        return false;
+    }
+
+    public function AllowNameChange()
+    {
+        return false;
+    }
+
+    public function AllowPhoneChange()
+    {
+        return false;
+    }
+
+    public function AllowOrganizationChange()
+    {
+        return false;
+    }
+
+    public function AllowPositionChange()
+    {
+        return false;
+    }
+}
