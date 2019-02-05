52d279a3df4d6fdb6bbefed2ce5b6068f160d773
Nick Korbel <nick@twinkletoessoftware.com>
2019-02-05 07:45:49 -0500

    bump to 2.7.5

--- lib/Config/Configuration.php
+++ lib/Config/Configuration.php
@@ -99,7 +99,7 @@ class Configuration implements IConfiguration
     const DEFAULT_CONFIG_ID = 'booked';
     const DEFAULT_CONFIG_FILE_PATH = 'config/config.php';
 
-    const VERSION = '2.7.4';
+    const VERSION = '2.7.5';
 
     protected function __construct()
     {
