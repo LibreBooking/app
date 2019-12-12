d9938663fcca9fa17cba9f583744af065769b2d5
Emanuel Garcês <effgarces@gmail.com>
2019-12-12 14:29:01 +0000

    Forgot to make available the pt_pt translation

--- lang/AvailableLanguages.php
+++ lang/AvailableLanguages.php
@@ -51,6 +51,7 @@ class AvailableLanguages
             'lt' => new AvailableLanguage('lt', 'lt.php', 'Lietuvių'),
             'no_no' => new AvailableLanguage('no_no', 'no_no.php', 'Norsk bokmål'),
             'pl' => new AvailableLanguage('pl', 'pl.php', 'Polski'),
+            'pt_pt' => new AvailableLanguage('pt_pt', 'pt_pt.php', 'Portugu&ecirc;s'),
             'pt_br' => new AvailableLanguage('pt_br', 'pt_br.php', 'Portugu&ecirc;s Brasileiro'),
             'ru_ru' => new AvailableLanguage('ru_ru', 'ru_ru.php', 'Русский'),
             'si_si' => new AvailableLanguage('si_si', 'si_si.php', 'Slovenščina'),
