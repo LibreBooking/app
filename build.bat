3c76f9cdce99bb5fa62d6c8440160c60591c16f3
attero <code@attero.me>
2021-04-20 12:46:16 +0200

    streamlined the build system for release packages

--- build.bat
+++ build.bat
@@ -1,11 +1,2 @@
 @echo off
-rem IF NOT EXIST c:\php\phing-2.16.3.phar GOTO PEAR_DIR
-vendor\phing\phing\bin\phing %1%
-
-
-:PEAR_DIR
-IF NOT EXIST c:\php\pear\phing.bat GOTO WAMP_DIR
-c:\php\pear\phing %1%
-
-:WAMP_DIR
-C:\wamp64\bin\php\php5.6.16\phing %1%
+phing %1%
--- config/config.devel.php
+++ config/config.devel.php
@@ -7,35 +7,35 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 /**
  * Application configuration
  */
-$conf['settings']['app.title'] = 'Booked Scheduler';			// application title
+$conf['settings']['app.title'] = 'Booked Scheduler';            // application title
 $conf['settings']['default.timezone'] = 'Etc/UTC';              // look up here http://php.net/manual/en/timezones.php
-$conf['settings']['allow.self.registration'] = 'true';         	// if users can register themselves
+$conf['settings']['allow.self.registration'] = 'true';          // if users can register themselves
 $conf['settings']['admin.email'] = 'admin@example.com';         // email address of admin user
-$conf['settings']['admin.email.name'] = 'Booked Administrator';	// name to be used in From: field when sending automatic emails
+$conf['settings']['admin.email.name'] = 'Booked Administrator'; // name to be used in From: field when sending automatic emails
 $conf['settings']['default.page.size'] = '50';                  // number of records per page
 $conf['settings']['enable.email'] = 'true';                     // global configuration to enable if any emails will be sent
 $conf['settings']['default.language'] = 'en_us';                // find your language in the lang directory
-$conf['settings']['script.url'] = '';   	                    // public URL to the Web directory of this instance. this is the URL that appears when you are logging in. leave http: or https: off to auto-detect
+$conf['settings']['script.url'] = '';                           // public URL to the Web directory of this instance. this is the URL that appears when you are logging in. leave http: or https: off to auto-detect
 $conf['settings']['image.upload.directory'] = 'Web/uploads/images'; // full or relative path to where images will be stored
 $conf['settings']['image.upload.url'] = 'uploads/images';       // full or relative path to show uploaded images from
-$conf['settings']['cache.templates'] = 'true';                  // true recommended, caching template files helps web pages render faster
-$conf['settings']['use.local.js.libs'] = 'false';                // false recommended, delivers jQuery from Google CDN, uses less bandwidth
+$conf['settings']['cache.templates'] = 'false';                 // true recommended, caching template files helps web pages render faster
+$conf['settings']['use.local.js.libs'] = 'false';               // false recommended, delivers jQuery from Google CDN, uses less bandwidth
 $conf['settings']['registration.captcha.enabled'] = 'true';     // recommended. unless using recaptcha this requires php_gd2 enabled in php.ini
-$conf['settings']['registration.require.email.activation'] = 'false';		// requires enable.email = true
-$conf['settings']['registration.auto.subscribe.email'] = 'false';			// requires enable.email = true
-$conf['settings']['registration.notify.admin'] = 'false';		// whether the registration of a new user sends an email to the admin (ala phpScheduleIt 1.2)
-$conf['settings']['inactivity.timeout'] = '30';     			// minutes before the user is automatically logged out
-$conf['settings']['name.format'] = '{first} {last}';     		// display format when showing user names
-$conf['settings']['css.extension.file'] = ''; 			      	// full or relative url to an additional css file to include. this can be used to override the default style
-$conf['settings']['disable.password.reset'] = 'false'; 	      	// if the password reset functionality should be disabled
-$conf['settings']['home.url'] = ''; 	      					// the url to open when the logo is clicked
-$conf['settings']['logout.url'] = ''; 	      					// the url to be directed to after logging out
-$conf['settings']['default.homepage'] = '1'; 	      			// the default homepage to use when new users register (1 = Dashboard, 2 = Schedule, 3 = My Calendar, 4 = Resource Calendar)
+$conf['settings']['registration.require.email.activation'] = 'false';       // requires enable.email = true
+$conf['settings']['registration.auto.subscribe.email'] = 'false';           // requires enable.email = true
+$conf['settings']['registration.notify.admin'] = 'false';       // whether the registration of a new user sends an email to the admin (ala phpScheduleIt 1.2)
+$conf['settings']['inactivity.timeout'] = '30';                 // minutes before the user is automatically logged out
+$conf['settings']['name.format'] = '{first} {last}';            // display format when showing user names
+$conf['settings']['css.extension.file'] = '';                   // full or relative url to an additional css file to include. this can be used to override the default style
+$conf['settings']['disable.password.reset'] = 'false';          // if the password reset functionality should be disabled
+$conf['settings']['home.url'] = '';                             // the url to open when the logo is clicked
+$conf['settings']['logout.url'] = '';                           // the url to be directed to after logging out
+$conf['settings']['default.homepage'] = '1';                    // the default homepage to use when new users register (1 = Dashboard, 2 = Schedule, 3 = My Calendar, 4 = Resource Calendar)
 
-$conf['settings']['schedule']['use.per.user.colors'] = 'false'; 		// color reservations by user
+$conf['settings']['schedule']['use.per.user.colors'] = 'false';         // color reservations by user
 $conf['settings']['schedule']['show.inaccessible.resources'] = 'true';  // whether or not resources that are inaccessible to the user are visible
-$conf['settings']['schedule']['reservation.label'] = '{name}';    		// format for what to display on the reservation slot label.  Available properties are: {name}, {title}, {description}, {email}, {phone}, {organization}, {position}, {startdate}, {enddate} {resourcename} {participants} {invitees} {reservationAttributes}. Custom attributes can be added using att with the attribute id. For example {att1}
-$conf['settings']['schedule']['hide.blocked.periods'] = 'false';    	// if blocked periods should be hidden or shown
+$conf['settings']['schedule']['reservation.label'] = '{name}';          // format for what to display on the reservation slot label.  Available properties are: {name}, {title}, {description}, {email}, {phone}, {organization}, {position}, {startdate}, {enddate} {resourcename} {participants} {invitees} {reservationAttributes}. Custom attributes can be added using att with the attribute id. For example {att1}
+$conf['settings']['schedule']['hide.blocked.periods'] = 'false';        // if blocked periods should be hidden or shown
 $conf['settings']['schedule']['update.highlight.minutes'] = '0';    // if set, will show reservations as 'updated' for a certain amount of time
 $conf['settings']['schedule']['show.week.numbers'] = 'false';
 /**
@@ -47,24 +47,24 @@ $conf['settings']['ics']['past.days'] = '0';
 /**
  * Privacy configuration
  */
-$conf['settings']['privacy']['view.schedules'] = 'true';       			// if unauthenticated users can view schedules
-$conf['settings']['privacy']['view.reservations'] = 'false';    			// if unauthenticated users can view reservations
-$conf['settings']['privacy']['hide.user.details'] = 'false';    			// if personal user details should be displayed to non-administrators
-$conf['settings']['privacy']['hide.reservation.details'] = 'false';			// if reservation details should be displayed to non-administrators. options are true, false, current, future, past
-$conf['settings']['privacy']['allow.guest.reservations'] = 'false';			// if reservations can be made by users without a Booked account, if true this overrides schedule and resource visibility
+$conf['settings']['privacy']['view.schedules'] = 'true';                // if unauthenticated users can view schedules
+$conf['settings']['privacy']['view.reservations'] = 'false';                // if unauthenticated users can view reservations
+$conf['settings']['privacy']['hide.user.details'] = 'false';                // if personal user details should be displayed to non-administrators
+$conf['settings']['privacy']['hide.reservation.details'] = 'false';         // if reservation details should be displayed to non-administrators. options are true, false, current, future, past
+$conf['settings']['privacy']['allow.guest.reservations'] = 'false';         // if reservations can be made by users without a Booked account, if true this overrides schedule and resource visibility
 /**
  * Reservation specific configuration
  */
-$conf['settings']['reservation']['start.time.constraint'] = 'future';		// when reservations can be created or edited. options are future, current, none
-$conf['settings']['reservation']['updates.require.approval'] = 'false';		// if updates to previously approved reservations require approval again
-$conf['settings']['reservation']['prevent.participation'] = 'false';		// if participation and invitation options should be removed
-$conf['settings']['reservation']['prevent.recurrence'] = 'false';			// if recurring reservations are disabled for non-administrators
-$conf['settings']['reservation']['enable.reminders'] = 'false';				// if reminders are enabled. this requires email to be enabled and the reminder job to be configured
+$conf['settings']['reservation']['start.time.constraint'] = 'future';       // when reservations can be created or edited. options are future, current, none
+$conf['settings']['reservation']['updates.require.approval'] = 'false';     // if updates to previously approved reservations require approval again
+$conf['settings']['reservation']['prevent.participation'] = 'false';        // if participation and invitation options should be removed
+$conf['settings']['reservation']['prevent.recurrence'] = 'false';           // if recurring reservations are disabled for non-administrators
+$conf['settings']['reservation']['enable.reminders'] = 'false';             // if reminders are enabled. this requires email to be enabled and the reminder job to be configured
 $conf['settings']['reservation']['allow.guest.participation'] = 'false';
 $conf['settings']['reservation']['allow.wait.list'] = 'false';
 $conf['settings']['reservation']['checkin.minutes.prior'] = '5';
-$conf['settings']['reservation']['default.start.reminder'] = '';			// the default start reservation reminder. format is ## interval. for example, 10 minutes, 2 hours, 6 days.
-$conf['settings']['reservation']['default.end.reminder'] = '';				// the default end reservation reminder. format is ## interval. for example, 10 minutes, 2 hours, 6 days.
+$conf['settings']['reservation']['default.start.reminder'] = '';            // the default start reservation reminder. format is ## interval. for example, 10 minutes, 2 hours, 6 days.
+$conf['settings']['reservation']['default.end.reminder'] = '';              // the default end reservation reminder. format is ## interval. for example, 10 minutes, 2 hours, 6 days.
 $conf['settings']['reservation']['title.required'] = 'false';
 $conf['settings']['reservation']['description.required'] = 'false';
 /**
@@ -85,25 +85,25 @@ $conf['settings']['reservation.notify']['group.admin.approval'] = 'false';
 /**
  * File upload configuration
  */
-$conf['settings']['uploads']['enable.reservation.attachments'] = 'false'; 	// if reservation attachments can be uploaded
-$conf['settings']['uploads']['reservation.attachment.path'] = 'uploads/reservation'; 	// full or relative (to the root of your installation) filesystem path to store reservation attachments
-$conf['settings']['uploads']['reservation.attachment.extensions'] = 'txt,jpg,gif,png,doc,docx,pdf,xls,xlsx,ppt,pptx,csv'; 	// comma separated list of file extensions that users are allowed to attach. leave empty to allow all extensions
+$conf['settings']['uploads']['enable.reservation.attachments'] = 'false';   // if reservation attachments can be uploaded
+$conf['settings']['uploads']['reservation.attachment.path'] = 'uploads/reservation';    // full or relative (to the root of your installation) filesystem path to store reservation attachments
+$conf['settings']['uploads']['reservation.attachment.extensions'] = 'txt,jpg,gif,png,doc,docx,pdf,xls,xlsx,ppt,pptx,csv';   // comma separated list of file extensions that users are allowed to attach. leave empty to allow all extensions
 /**
  * Database configuration
  */
 $conf['settings']['database']['type'] = 'mysql';
-$conf['settings']['database']['user'] = 'booked_user';        // database user with permission to the booked database
-$conf['settings']['database']['password'] = 'password';
-$conf['settings']['database']['hostspec'] = '127.0.0.1';        // ip, dns or named pipe
+$conf['settings']['database']['user'] = 'test';        // database user with permission to the booked database
+$conf['settings']['database']['password'] = 'test';
+$conf['settings']['database']['hostspec'] = '127.0.0.1';      // ip, dns or named pipe
 $conf['settings']['database']['name'] = 'bookedscheduler';
 /**
  * Mail server configuration
  */
 $conf['settings']['phpmailer']['mailer'] = 'smtp';              // options are 'mail', 'smtp' or 'sendmail'
-$conf['settings']['phpmailer']['smtp.host'] = '';               // 'smtp.company.com'
+$conf['settings']['phpmailer']['smtp.host'] = 'localhost';               // 'smtp.company.com'
 $conf['settings']['phpmailer']['smtp.port'] = '25';
 $conf['settings']['phpmailer']['smtp.secure'] = '';             // options are '', 'ssl' or 'tls'
-$conf['settings']['phpmailer']['smtp.auth'] = 'true';           // options are 'true' or 'false'
+$conf['settings']['phpmailer']['smtp.auth'] = 'false';           // options are 'true' or 'false'
 $conf['settings']['phpmailer']['smtp.username'] = '';
 $conf['settings']['phpmailer']['smtp.password'] = '';
 $conf['settings']['phpmailer']['sendmail.path'] = '/usr/sbin/sendmail';
