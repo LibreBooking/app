ed429bd92d308a86a4f9b948e58826cce87dbe41
Robin ALEXANDER <64310405+colisee@users.noreply.github.com>
2022-09-05 09:16:31 +0200

    Fix SQL user creation
    There is an SQL user name inconsistency between the files:
    - Presenters/Install/Installer.php: which uses schedule_user
    - database_schema/create-user.sql: which uses lb_user
    
    I chose to modify the file create-user.sql, but one could decide to modify the file Installer.php

--- database_schema/create-user.sql
+++ database_schema/create-user.sql
@@ -1,8 +1,8 @@
-DROP USER IF EXISTS 'lb_user'@'localhost';
-DROP USER IF EXISTS 'lb_user'@'127.0.0.1';
+DROP USER IF EXISTS 'schedule_user'@'localhost';
+DROP USER IF EXISTS 'schedule_user'@'127.0.0.1';
 
-CREATE USER 'lb_user'@'localhost' identified by 'password';
-CREATE USER 'lb_user'@'127.0.0.1' identified by 'password';
+CREATE USER 'schedule_user'@'localhost' identified by 'password';
+CREATE USER 'schedule_user'@'127.0.0.1' identified by 'password';
 
-GRANT ALL on librebooking.* to 'lb_user'@'localhost';
-GRANT ALL on librebooking.* to 'lb_user'@'127.0.0.1';
+GRANT ALL on librebooking.* to 'schedule_user'@'localhost';
+GRANT ALL on librebooking.* to 'schedule_user'@'127.0.0.1';
