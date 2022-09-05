c36f086952294a8fb81475c590354916303ac91a
Robin ALEXANDER <64310405+colisee@users.noreply.github.com>
2022-09-05 09:24:49 +0200

    Fix SQL user creation (ip address)
    The Presenters/Install/Installer.php file will create 2 SQL users, with 2 different ip addresses:
    - 127.0.0.1
    - database ip address
    
    This is incorrect when the database and web servers are on 2 different ip addresses (ex: when the database and web servers are on 2 separate containers).
    One could fix the Installer.php file to use the web server ip address, but it is preferable to use the wildcard ip address "%", for it will continue to work in the case that the web server ip address changes.

--- database_schema/create-user.sql
+++ database_schema/create-user.sql
@@ -1,8 +1,8 @@
-DROP USER IF EXISTS 'schedule_user'@'localhost';
+DROP USER IF EXISTS 'schedule_user'@'%';
 DROP USER IF EXISTS 'schedule_user'@'127.0.0.1';
 
-CREATE USER 'schedule_user'@'localhost' identified by 'password';
+CREATE USER 'schedule_user'@'%' identified by 'password';
 CREATE USER 'schedule_user'@'127.0.0.1' identified by 'password';
 
-GRANT ALL on librebooking.* to 'schedule_user'@'localhost';
+GRANT ALL on librebooking.* to 'schedule_user'@'%';
 GRANT ALL on librebooking.* to 'schedule_user'@'127.0.0.1';
