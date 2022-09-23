144c2072775d5a527837009838fd91d44d0e5696
effgarces <effgarces@gmail.com>
2022-09-23 09:37:45 +0100

    Fix SQL user creation
    As reported by @colisee on #138 there's an issue when the database and web server are on 2 different ip addresses.  As sugested, a wildcard has been added, to allow for web server ip address changes. All credits go to @colisee

--- database_schema/create-user.sql
+++ database_schema/create-user.sql
@@ -1,8 +1,8 @@
-DROP USER IF EXISTS 'lb_user'@'localhost';
-CREATE USER 'lb_user'@'localhost' identified by 'password';
+DROP USER IF EXISTS 'lb_user'@'%';
+CREATE USER 'lb_user'@'%' identified by 'password';
 
 DROP USER IF EXISTS 'lb_user'@'127.0.0.1';
 CREATE USER 'lb_user'@'127.0.0.1' identified by 'password';
 
-GRANT ALL on librebooking.* to 'lb_user'@'localhost';
+GRANT ALL on librebooking.* to 'lb_user'@'%';
 GRANT ALL on librebooking.* to 'lb_user'@'127.0.0.1';
