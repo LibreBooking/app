0a785d7e6213213c84db6b9e51bbc8602fea0ae4
ramartins02 <randrademartins02@gmail.com>
2023-11-16 09:55:14 +0000

    refactor: added comments

--- Web/microsoft-auth.php
+++ Web/microsoft-auth.php
@@ -3,7 +3,7 @@ define('ROOT_DIR', '../');
 
 require_once(ROOT_DIR . 'lib/Common/namespace.php');
 
-//Checks if the user was authenticated by microsoft and redirects to external login page
+//Checks if the user was authenticated by microsoft and redirects to external authentication page
 if (isset($_GET['code'])) {
     $code = filter_input(INPUT_GET, 'code');
     header("Location: ".ROOT_DIR."Web/external-auth.php?type=microsoft&code=".$code);
