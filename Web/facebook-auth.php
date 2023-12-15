d6678caa6d4eb9b897172b2150fcb48c80b42d9c
ramartins02 <randrademartins02@gmail.com>
2023-12-15 14:06:55 +0000

    fix(facebook-auth): forgot variable in catch

--- Web/facebook-auth.php
+++ Web/facebook-auth.php
@@ -21,7 +21,7 @@ if (isset($_GET['code'])) {
         $code = filter_input(INPUT_GET,'code');
         header("Location: ".ROOT_DIR."Web/external-auth.php?type=fb&code=".$code);
         exit;
-    } catch (\Facebook\Exceptions\FacebookResponseException | \Facebook\Exceptions\FacebookSDKException) {
+    } catch (\Facebook\Exceptions\FacebookResponseException | \Facebook\Exceptions\FacebookSDKException $e) {
         Log::Debug("Exception during facebook login: %s", $e->getMessage());
         $_SESSION['facebook_error'] = true;
         header("Location:".ROOT_DIR."Web");
